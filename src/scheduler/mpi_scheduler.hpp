77fa583644617f5c972e355c32ed45f75d716ee3
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2022-01-18 21:17:12 +0100

    using v..._allgather instead of v..._allgather_ks

--- src/scheduler/mpi_scheduler.hpp
+++ src/scheduler/mpi_scheduler.hpp
@@ -3,8 +3,12 @@
 #include "patch.hpp"
 #include "../sys/mpi_handler.hpp"
 #include <map>
-#include <mpi.h>
+#include <unordered_map>
 #include <vector>
+
+struct PatchData{
+    u64 a;
+};
 
 class MpiScheduler{public:
 
@@ -12,7 +16,15 @@ class MpiScheduler{public:
     std::vector<Patch> local_patch_list;
 
     inline void sync_cluster_patches(){
-        mpi_handler::vector_allgatherv_ks(local_patch_list, patch_MPI_type, global_patch_list, patch_MPI_type, MPI_COMM_WORLD);
+        mpi_handler::vector_allgatherv(local_patch_list, patch_MPI_type, global_patch_list, patch_MPI_type, MPI_COMM_WORLD);
+    }
+
+
+
+
+    
+    inline void build_local_patch_list(){
+        
     }
 
     inline MpiScheduler(){
