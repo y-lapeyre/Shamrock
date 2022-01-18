a17a33f36c853af9a48204e16eeabf16dd76c968
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2022-01-18 21:16:07 +0100

    vector_allgrather mpi primitive

--- src/sys/mpi_handler.hpp
+++ src/sys/mpi_handler.hpp
@@ -111,6 +111,69 @@ namespace mpi_handler{
 
 
 
+    /**
+     * @brief allgatherv on vector with size query (size querrying variant of vector_allgatherv_ks)
+     * //TODO add fault tolerance
+     * @tparam T 
+     * @param send_vec 
+     * @param send_type 
+     * @param recv_vec 
+     * @param recv_type 
+     */
+    template<class T>
+    inline void vector_allgatherv(const std::vector<T> & send_vec ,const MPI_Datatype send_type,std::vector<T> & recv_vec,const MPI_Datatype recv_type,const MPI_Comm comm){
+
+        u32 local_count = send_vec.size();
+
+
+        //querry global size and resize the receiving vector
+        u32 global_len;
+        mpi::allreduce(&local_count, &global_len, 1, MPI_INT , MPI_SUM, MPI_COMM_WORLD);
+        recv_vec.resize(global_len);
+
+
+
+        int* table_data_count = new int[world_size];
+
+        mpi::allgather(
+            &local_count, 
+            1, 
+            MPI_INT, 
+            &table_data_count[0], 
+            1, 
+            MPI_INT, 
+            comm);
+
+        //printf("table_data_count = [%d,%d,%d,%d]\n",table_data_count[0],table_data_count[1],table_data_count[2],table_data_count[3]);
+
+
+
+        int* node_displacments_data_table = new int[world_size];
+
+        node_displacments_data_table[0] = 0;
+
+        for(u32 i = 1 ; i < world_size; i++){
+            node_displacments_data_table[i] = node_displacments_data_table[i-1] + table_data_count[i-1];
+        }
+        
+        //printf("node_displacments_data_table = [%d,%d,%d,%d]\n",node_displacments_data_table[0],node_displacments_data_table[1],node_displacments_data_table[2],node_displacments_data_table[3]);
+        
+        mpi::allgatherv(
+            &send_vec[0], 
+            send_vec.size(),
+            send_type, 
+            &recv_vec[0], 
+            table_data_count, 
+            node_displacments_data_table, 
+            recv_type, 
+            comm);
+
+
+        delete [] table_data_count;
+        delete [] node_displacments_data_table;
+    } 
+
+
 
 
     /**
--- src/unittests/sys/test_mpi_handler.cpp
+++ src/unittests/sys/test_mpi_handler.cpp
@@ -46,6 +46,49 @@ Test_start("mpi::",vector_allgather_ks,-1){
 
 
 
+Test_start("mpi::",vector_allgather,-1){
+
+    //in the end this vector should be recovered in recv_vec
+    std::vector<u32> check_vec(mpi_handler::world_size*11);
+    {
+        //fill the check vector with a pseudo random int generator (seed:0x1111)
+        std::mt19937 eng(0x1111);                                     
+        std::uniform_int_distribution<u32> dist(0,4294967295);
+
+        for (u32 & element : check_vec) {
+            element = dist(eng);
+        }
+    }
+
+    //divide the check_vec in local_vector on each node
+    std::vector<u32> local_vector;
+    {
+        std::vector<u32> pointer_start_node(mpi_handler::world_size);
+        pointer_start_node[0] = 0;
+        for(u32 i = 1; i < mpi_handler::world_size; i ++){
+            pointer_start_node[i] = pointer_start_node[i-1] +5+ ((i-1)%5)*((i-1)%5);
+        }
+        pointer_start_node.push_back(mpi_handler::world_size*11);
+
+        for(u32 id = pointer_start_node[mpi_handler::world_rank]; id < pointer_start_node[mpi_handler::world_rank+1]; id ++){
+            local_vector.push_back(check_vec[id]);
+        }
+    }
+
+    //receive data
+    std::vector<u32> recv_vec;
+    mpi_handler::vector_allgatherv(local_vector, MPI_INT, recv_vec, MPI_INT, MPI_COMM_WORLD);
+
+    Test_assert("vector lenght correct", recv_vec.size() == mpi_handler::world_size*11);
+    //check correctness
+    for(u32 i = 1; i < mpi_handler::world_size*11; i ++) {
+        Test_assert(format("check_vec[%d] == recv_vec[%d]",i,i), check_vec[i] == recv_vec[i]);
+    }
+
+}
+
+
+
 
 
 template<class T>
