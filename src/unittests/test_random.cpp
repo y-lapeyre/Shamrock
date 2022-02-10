33107f064400b4cd791a984c4349901886dc573f
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2022-02-10 18:09:21 +0100

    integrator polymorphism test

--- src/unittests/test_random.cpp
+++ src/unittests/test_random.cpp
@@ -1,5 +1,7 @@
 #include "shamrocktest.hpp"
 
+#include "../sys/sycl_handler.hpp"
+#include <vector>
 
 Test_start("",intmult,1){
     int a = 3;
@@ -38,5 +40,84 @@ Test_start("",test_overload_new,2){
     Test_assert_log("int multiplication log", *a==6,"wesh");
 
     delete a;
+
+}
+
+
+class ForcePressure{public:
+    static int f(int i){
+        return 2*i;
+    }
+};
+
+class ForcePressure2{public:
+    static int f(int i){
+        return 4*i;
+    }
+};
+
+
+namespace integrator {
+
+    template<class ForceFunc>
+    class Leapfrog{public:
+
+        void step(std::vector<i32> & fres_arr){
+            
+            cl::sycl::buffer<i32> fres(fres_arr);
+
+            cl::sycl::range<1> range{10};
+
+            queue->submit([&](cl::sycl::handler &cgh) {
+                auto ff = fres.get_access<sycl::access::mode::read_write>(cgh);
+
+                cgh.parallel_for<class Write_chosen_node>(range, [=](cl::sycl::item<1> item) {
+                    u64 i = (u64)item.get_id(0);
+                    ff[i] = ForceFunc::f(ff[i]);
+                });
+            });
+            
+        }
+
+    };
+}
+
+typedef void (*stepper_main_func_t)(std::vector<int> &) ;
+static integrator::Leapfrog<ForcePressure> integLeapPres;
+class Stepper{public:
+    integrator::Leapfrog<ForcePressure> stepper_func;
+
+    
+};
+
+
+Test_start("",sycl_static_func,1){
+
+    init_sycl();
+
+    std::vector<i32> fres_arr = {0,8,4,1,8,7,2,3,77,1};
+    
+
+    std::vector<void (*)(std::vector<int> &)> timesteppers;
+    
+    timesteppers.push_back(integrator::Leapfrog<ForcePressure>::step);
+    timesteppers.push_back(integrator::Leapfrog<ForcePressure2>::step);
+
+
+    timesteppers[0](fres_arr);
+
+    timesteppers[1](fres_arr);
+
+    std::cout << fres_arr[0] << std::endl;
+    std::cout << fres_arr[1] << std::endl;
+    std::cout << fres_arr[2] << std::endl;
+    std::cout << fres_arr[3] << std::endl;
+    std::cout << fres_arr[4] << std::endl;
+    std::cout << fres_arr[5] << std::endl;
+    std::cout << fres_arr[6] << std::endl;
+    std::cout << fres_arr[7] << std::endl;
+    std::cout << fres_arr[8] << std::endl;
+    std::cout << fres_arr[9] << std::endl;
+
 
 }
