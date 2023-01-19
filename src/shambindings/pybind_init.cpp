49f5e208e0e47edbb65d95aa269e4fd81aa3990f
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-01-19 18:35:46 +0100

    redirect std::cout to python if built for pip

--- src/shambindings/pybind_init.cpp
+++ src/shambindings/pybind_init.cpp
@@ -1,8 +1,18 @@
 #include "pybindaliases.hpp"
 
+#include <pybind11/iostream.h>
 
 
 SHAMROCK_PY_MODULE(shamrock,m){
+
+    #ifdef LIB_BUILD
+    m.attr("redirect_output") = py::capsule(new py::scoped_ostream_redirect(
+        std::cout,                               // std::ostream&
+        py::module_::import("sys").attr("stdout") // Python output
+    ),
+    [](void *sor) { delete static_cast<py::scoped_ostream_redirect *>(sor); });
+    #endif
+
     for(auto fct : static_init_shamrock_pybind){
         fct(m);
     }
