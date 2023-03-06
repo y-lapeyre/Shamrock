5662b0998d34878b8b0e059fc100762be1e86f8a
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-03-06 15:51:51 +0100

    [Doc] copy to new build folder after doc gen

--- doc/make_doc.sh
+++ doc/make_doc.sh
@@ -6,3 +6,23 @@ jupyter-book build guide_cpp
 jupyter-book build guide_sycl
 jupyter-book build shamrock_dev_doc
 jupyter-book build shamrock_doc
+
+rm -rf _build
+mkdir _build
+cd _build
+
+
+mkdir guide_cpp
+mkdir guide_sycl
+mkdir shamrock_dev_doc
+mkdir shamrock_doc
+
+mkdir doxygen
+
+cp -r ../guide_cpp/_build/* guide_cpp
+cp -r ../guide_sycl/_build/* guide_sycl
+cp -r ../shamrock_dev_doc/_build/* shamrock_dev_doc
+cp -r ../shamrock_doc/_build/* shamrock_doc
+
+
+cp -r ../doxygen/html doxygen
