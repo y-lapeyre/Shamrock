8340afa91293fc37d57a6d808e7d62ebf6881570
tdavidcl <timothee.davidcleri@gmail.com>
2023-03-11 14:19:26 +0100

    [Legacy] remove old string format

--- src/shamrock/legacy/utils/string_utils.hpp
+++ src/shamrock/legacy/utils/string_utils.hpp
@@ -25,20 +25,7 @@
 #include "shambase/stringUtils.hpp"
 #include <cstdarg>
 
-[[deprecated]]
-inline std::string format(const char *fmt...) {
-    std::string s{};
-    va_list args, args2;
-    va_start(args, fmt);
-    va_copy(args2, args);
 
-    s.resize(vsnprintf(nullptr, 0, fmt, args2) + 1);
-    va_end(args2);
-    vsprintf(s.data(), fmt, args);
-    va_end(args);
-    s.pop_back();
-    return s;
-}
 
 inline void write_string_to_file(std::string filename, std::string s) {
     std::ofstream myfile;
