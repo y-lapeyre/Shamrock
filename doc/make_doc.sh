405b971d143a7a3ce5102223ebdcac7143c79321
tdavidcl <timothee.davidcleri@gmail.com>
2023-03-11 23:52:58 +0100

    [Doc] add mdbook doc

--- doc/make_doc.sh
+++ doc/make_doc.sh
@@ -1,30 +1,23 @@
 cd doxygen
 doxygen dox.conf
 
+cd ../shamrock-doc
+mkdir bin
+curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.4.28/mdbook-v0.4.28-x86_64-unknown-linux-gnu.tar.gz | tar -xz --directory=bin
+bin/mdbook build
+
 cd ..
-jupyter-book build guide_cpp
-jupyter-book build guide_sycl
-jupyter-book build shamrock_dev_doc
-jupyter-book build shamrock_doc
+
 
 rm -rf _build
 mkdir _build
 cd _build
 
 
-mkdir guide_cpp
-mkdir guide_sycl
-mkdir shamrock_dev_doc
-mkdir shamrock_doc
-
+mkdir mdbook
 mkdir doxygen
 
-cp -r ../guide_cpp/_build/html/* guide_cpp
-cp -r ../guide_sycl/_build/html/* guide_sycl
-cp -r ../shamrock_dev_doc/_build/html/* shamrock_dev_doc
-cp -r ../shamrock_doc/_build/html/* shamrock_doc
-
-
+cp -r ../shamrock-doc/book/* mdbook
 cp -r ../doxygen/html/* doxygen
 
 
---
+++ doc/shamrock-doc/src/SUMMARY.md
@@ -0,0 +1,20 @@
+# Summary
+
+# User Documentation
+
+- [QuickStart](./usermanual/quickstart.md)
+
+# Develloper Doc
+
+- [Codebase Organisation](./devdoc/codebase_org.md)
+- [Code coverage](./devdoc/code_coverage.md)
+
+# Sycl guide
+
+- [SYCL tricks](./syclguide/sycl_tricks.md)
+
+# C++ guide
+
+- [Replacing preprocessors](./cppguide/replacing_preproc.md)
+- [Low level tricks](./cppguide/low_level_tricks.md)
+- [Dirty tricks](./cppguide/dirty_tricks.md)
