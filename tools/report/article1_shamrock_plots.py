b6889e5837f2add87a09cf01858f1a6cc81158a0
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-03-31 21:17:31 +0200

    [Bench] update plots

--- tools/report/article1_shamrock_plots.py
+++ tools/report/article1_shamrock_plots.py
@@ -28,14 +28,14 @@ def standalone(json_lst : list, figure_folder : str) -> str:
 
         build_perf = res.get_test_instances("Benchmark","shamrock_article1:tree_build_perf")
 
-        fileprefix = str(i)
+        
 
         if len(build_perf) == 1:
 
             Nparts = []
             results = {}
 
-            fig,axs = plt.subplots(nrows=1,ncols=1,figsize=(7,6))  
+            
 
             for s in build_perf:
 
@@ -43,49 +43,52 @@ def standalone(json_lst : list, figure_folder : str) -> str:
 
                     n = dataset["dataset_name"]    
 
-                    if n == "morton = u32, field type = f32":
-
-                        vecN = np.array(s.get_test_dataset(n,"Npart")[1::])
-                        
-                        #the first point is wrong because a buffer of size 1e8 is moved
-                        plt.plot(vecN, np.array(s.get_test_dataset(n,"times_full_tree"          )[1::])/vecN, label = "total build time",color='black',linewidth = 2)
-                        #plt.plot(vecN, np.array(s.get_test_dataset(n,"times_morton"             )[1::])/vecN, label = "morton list build",color='black',linestyle="--")
-                        plt.plot(vecN, np.array(s.get_test_dataset(n,"times_morton_build"       )[1::])/vecN, label = "compute morton")
-                        plt.plot(vecN, np.array(s.get_test_dataset(n,"times_morton_sort"        )[1::])/vecN, label = "bitonic sort",color='black',linestyle="--")
-                        plt.plot(vecN, np.array(s.get_test_dataset(n,"times_reduc"              )[1::])/vecN, label = "remove duplicates")
-                        plt.plot(vecN, np.array(s.get_test_dataset(n,"times_karras"             )[1::])/vecN, label = "T. Karras 2012")
-                        plt.plot(vecN, np.array(s.get_test_dataset(n,"times_compute_int_range"  )[1::])/vecN, label = "compute ranges")
-                        plt.plot(vecN, np.array(s.get_test_dataset(n,"times_compute_coord_range")[1::])/vecN, label = "convert ranges")
-                        #plt.plot(vecN, np.array(s.get_test_dataset(n,"times_trailling_fill"     )[1::])/vecN, label = "tralling index fill")
-                        #plt.plot(vecN, np.array(s.get_test_dataset(n,"times_index_gen"          )[1::])/vecN, label = "index table gen")
+                    fileprefix = str(i)
 
 
-            axs.set_xscale('log')
-            axs.set_yscale('log')
-            axs.set_xlabel(r"$N$")
-            axs.set_ylabel(r"$t_{\rm build} /N (s)$")
-            axs.legend()
-            axs.grid()
-            plt.tight_layout()
-            plt.savefig(figure_folder+fileprefix+"article1_shamrock_build_perf_tree_random.pdf")
+                    fig,axs = plt.subplots(nrows=1,ncols=1,figsize=(7,6))  
+                    vecN = np.array(s.get_test_dataset(n,"Npart")[1::])
+                    
+                    #the first point is wrong because a buffer of size 1e8 is moved
+                    plt.plot(vecN, np.array(s.get_test_dataset(n,"times_full_tree"          )[1::])/vecN, label = "total build time",color='black',linewidth = 2)
+                    #plt.plot(vecN, np.array(s.get_test_dataset(n,"times_morton"             )[1::])/vecN, label = "morton list build",color='black',linestyle="--")
+                    plt.plot(vecN, np.array(s.get_test_dataset(n,"times_morton_build"       )[1::])/vecN, label = "compute morton")
+                    plt.plot(vecN, np.array(s.get_test_dataset(n,"times_morton_sort"        )[1::])/vecN, label = "bitonic sort",color='black',linestyle="--")
+                    plt.plot(vecN, np.array(s.get_test_dataset(n,"times_reduc"              )[1::])/vecN, label = "remove duplicates")
+                    plt.plot(vecN, np.array(s.get_test_dataset(n,"times_karras"             )[1::])/vecN, label = "T. Karras 2012")
+                    plt.plot(vecN, np.array(s.get_test_dataset(n,"times_compute_int_range"  )[1::])/vecN, label = "compute ranges")
+                    plt.plot(vecN, np.array(s.get_test_dataset(n,"times_compute_coord_range")[1::])/vecN, label = "convert ranges")
+                    #plt.plot(vecN, np.array(s.get_test_dataset(n,"times_trailling_fill"     )[1::])/vecN, label = "tralling index fill")
+                    #plt.plot(vecN, np.array(s.get_test_dataset(n,"times_index_gen"          )[1::])/vecN, label = "index table gen")
 
 
-
-            buf += res.get_config_str()
-            buf +=  r"""
-
-            \begin{figure}[ht!]
-            \center
-            \includegraphics[width=0.8\textwidth]{"""+ "figures/"+fileprefix+"article1_shamrock_build_perf_tree_random.pdf" + r"""}
-            \caption{TODO}
-            \label{fig:fmm_prec}
-            \end{figure}
-
-
-            """
+                    axs.set_xscale('log')
+                    axs.set_yscale('log')
+                    axs.set_xlabel(r"$N$")
+                    axs.set_ylabel(r"$t_{\rm build} /N (s)$")
+                    axs.legend()
+                    axs.grid()
+                    plt.tight_layout()
+                    plt.savefig(figure_folder+fileprefix+"article1_shamrock_build_perf_tree_random.pdf")
 
 
 
+                    buf += res.get_config_str()
+                    buf +=  r"""
+
+                    \begin{figure}[ht!]
+                    \center
+                    \includegraphics[width=0.8\textwidth]{"""+ "figures/"+fileprefix+"article1_shamrock_build_perf_tree_random.pdf" + r"""}
+                    \caption{"""+n + r"""}
+                    \label{fig:fmm_prec}
+                    \end{figure}
+
+
+                    """
+                    i += 1
+
+
+            fileprefix = str(i)
 
             vec_Npart = 0
             dic_ = {}
