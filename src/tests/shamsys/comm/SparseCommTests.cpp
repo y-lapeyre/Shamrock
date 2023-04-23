7be9a53d7903be44b226b5d24424754e52ced22c
tdavidcl <timothee.davidcleri@gmail.com>
2023-04-23 17:31:51 +0200

    [Comm] prepare test for sparse comm

---
+++ src/tests/shamsys/comm/SparseCommTests.cpp
@@ -0,0 +1,15 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright(C) 2021-2023 Timothée David--Cléris <timothee.david--cleris@ens-lyon.fr>
+// Licensed under CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+#include "shamsys/comm/SparseCommunicator.hpp"
+#include "shamtest/details/TestResult.hpp"
+#include "shamtest/shamtest.hpp"
+
+TestStart(Unittest, "name", testsparsecomm, -1){
+    
+}
