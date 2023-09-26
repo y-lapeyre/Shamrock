a2634654dd158805a57fd7da555c2f838f12b663
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-09-26 17:29:10 +0200

    [IO] add missing liscence

--- src/shamrock/io/AsciiSplitDump.cpp
+++ src/shamrock/io/AsciiSplitDump.cpp
@@ -1,3 +1,11 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright(C) 2021-2023 Timothée David--Cléris <timothee.david--cleris@ens-lyon.fr>
+// Licensed under CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
 #include "AsciiSplitDump.hpp"
 
 template<class T>
