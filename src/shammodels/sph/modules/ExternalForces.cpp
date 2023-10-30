226c60c6ef73476c3e98329a7ec292757745cd5b
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-10-30 18:51:26 +0100

    [Doc] fix doxygen header

--- src/shammodels/sph/modules/ExternalForces.cpp
+++ src/shammodels/sph/modules/ExternalForces.cpp
@@ -7,7 +7,7 @@
 // -------------------------------------------------------//
 
 /**
- * @file ConservativeCheck.cpp
+ * @file ExternalForces.cpp
  * @author Timothée David--Cléris (timothee.david--cleris@ens-lyon.fr)
  * @brief
  *
