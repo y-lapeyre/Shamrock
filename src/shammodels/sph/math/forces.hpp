6b7e3b8001d40f076adf86e52deb0b993c1a7667
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-11-04 10:40:52 +1100

    [SPH] fix vsig_u

--- src/shammodels/sph/math/forces.hpp
+++ src/shammodels/sph/math/forces.hpp
@@ -14,6 +14,7 @@
  * @brief file containing formulas for sph forces
  */
 
+#include "shambackends/math.hpp"
 #include "shambase/sycl_utils/sycl_utilities.hpp"
 namespace shamrock::sph {
 
@@ -208,7 +209,12 @@ namespace shamrock::sph {
         //  scalar : f32  | vector : f32_3
         Tscal vsig_a = alpha_a * cs_a + beta_AV * abs_v_ab_r_ab;
         Tscal vsig_b = alpha_b * cs_b + beta_AV * abs_v_ab_r_ab;
-        Tscal vsig_u = abs_v_ab_r_ab;
+
+
+        //Tscal vsig_u = abs_v_ab_r_ab;
+        Tscal rho_avg = (rho_a + rho_b)*0.5;
+        Tscal abs_dp = sham::abs(P_a - P_b);
+        Tscal vsig_u = sycl::sqrt(abs_dp/rho_avg);
 
         Tscal dWab_a = Fab_a;
         Tscal dWab_b = Fab_b;
