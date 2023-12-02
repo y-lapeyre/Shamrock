171d28d365e795b0d8866a68cb83bbcc3a2a0362
tdavidcl <timothee.davidcleri@gmail.com>
2023-12-02 11:56:46 +0100

    [Doc] fix doxygen header

--- src/shambindings/valarray_fix.hpp
+++ src/shambindings/valarray_fix.hpp
@@ -1,4 +1,20 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright(C) 2021-2023 Timothée David--Cléris <timothee.david--cleris@ens-lyon.fr>
+// Licensed under CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+
 #pragma once
+
+/**
+ * @file valarray_fix.hpp
+ * @author Timothée David--Cléris (timothee.david--cleris@ens-lyon.fr)
+ * @brief 
+ * 
+ */
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wkeyword-macro"
