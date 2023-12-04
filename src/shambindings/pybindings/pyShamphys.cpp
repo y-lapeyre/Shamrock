514a386f9b7b652a0ffb4eec140eb07e25450efd
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-12-04 17:22:31 +0100

    [Phys] fix doxygen header

--- src/shambindings/pybindings/pyShamphys.cpp
+++ src/shambindings/pybindings/pyShamphys.cpp
@@ -7,7 +7,7 @@
 // -------------------------------------------------------//
 
 /**
- * @file pySfc.cpp
+ * @file pyShamphys.cpp
  * @author Timothée David--Cléris (timothee.david--cleris@ens-lyon.fr)
  * @brief
  */
