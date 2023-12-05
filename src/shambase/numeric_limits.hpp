8a0d4a07bd6676e1e9e42f04995dfbc6f81e49c3
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-12-05 11:04:05 +0100

    [Base] implement primitive_type_info

--- src/shambase/numeric_limits.hpp
+++ src/shambase/numeric_limits.hpp
@@ -9,56 +9,44 @@
 #pragma once
 
 /**
- * @file memory.hpp
+ * @file numeric_limits.hpp
  * @author Timothée David--Cléris (timothee.david--cleris@ens-lyon.fr)
  * @brief 
  * 
  */
  
 #include "shambase/aliases_float.hpp"
+#include "shambase/primitive_type_info.hpp"
 #include <limits>
 
 namespace shambase{
 
+
     template<class T>
-    struct primitive_type_info{
-        static constexpr bool implemented = false;
-    }
-
-    namespace details {
-
-        template<class T>
-        struct numeric_limits{
-            static constexpr bool implemented = false;
-            
-            inline constexpr static T max(){return {};}
-            inline constexpr static T min(){return {};}
-            inline constexpr static T epsilon(){return {};}
-            inline constexpr static T infty(){return {};}
-            inline constexpr static T infty_neg(){return {};}
-            inline constexpr static T zero(){return {};}
-        };
-
-        template<>
-        struct numeric_limits<f64>{
-            using T = f64;
-            static constexpr bool implemented = true;
-            
-            inline constexpr static T max(){return std::numeric_limits<T>::max();}
-            inline constexpr static T min(){return {};}
-            inline constexpr static T epsilon(){return {};}
-            inline constexpr static T infty(){return {};}
-            inline constexpr static T infty_neg(){return {};}
-            inline constexpr static T zero(){return {};}
-        };
-
-
+    constexpr T get_max(){
+        static_assert(primitive_type_info<T>::is_specialized, "this type was not implemented in shambase::details::numeric_limits" );
+        return primitive_type_info<T>::max;
     }
 
     template<class T>
-    static constexpr T get_max(){
-        static_assert(details::numeric_limits<T>::implemented, "this type was not implemented in shambase::details::numeric_limits" );
-        return details::numeric_limits<T>::max();
+    constexpr T get_min(){
+        static_assert(primitive_type_info<T>::is_specialized, "this type was not implemented in shambase::details::numeric_limits" );
+        return primitive_type_info<T>::min;
+    }
+
+
+    template<class T>
+    constexpr T get_epsilon(){
+        static_assert(primitive_type_info<T>::is_specialized, "this type was not implemented in shambase::details::numeric_limits" );
+        static_assert(primitive_type_info<T>::is_float, "this function can only be called on floats");
+        return primitive_type_info<T>::epsilon;
+    }
+
+    template<class T>
+    constexpr T get_infty(){
+        static_assert(primitive_type_info<T>::is_specialized, "this type was not implemented in shambase::details::numeric_limits" );
+        static_assert(primitive_type_info<T>::is_float, "this function can only be called on floats");
+        return primitive_type_info<T>::infty;
     }
 
 }
