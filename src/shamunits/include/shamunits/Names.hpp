fc5d3baad5fd71ed892ad442ec3992ac6b52b173
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-12-06 18:15:46 +0100

    [Doc] add a package to the ci mkdocs run

--- src/shamunits/include/shamunits/Names.hpp
+++ src/shamunits/include/shamunits/Names.hpp
@@ -57,7 +57,8 @@
     X1(light_year, ly)                                                                             \
     X1(parsec, pc)                                                                                 \
     X1(electron_volt, eV)                                                                          \
-    X1(ergs, erg)
+    X1(ergs, erg)                                                                          \
+    X1(british_pint, pint)
 
 #define XMAC_UNIT_PREFIX  \
     X(tera  ,T, 12)\
