c7eb501aca1f4184ae87bc72f0d30669d5e1b364
Guillet, Thomas <T.A.Guillet@exeter.ac.uk>
2023-12-22 11:04:19 +0100

    fix copyright notice

--- src/shammath/riemann.hpp
+++ src/shammath/riemann.hpp
@@ -1,9 +1,8 @@
 // -------------------------------------------------------//
 //
 // SHAMROCK code for hydrodynamics
-// Copyright(C) 2021-2023 Timothée David--Cléris
-// <timothee.david--cleris@ens-lyon.fr> Licensed under CeCILL 2.1 License, see
-// LICENSE for more information
+// Copyright(C) 2021-2023 Timothée David--Cléris <timothee.david--cleris@ens-lyon.fr>
+// Licensed under CeCILL 2.1 License, see LICENSE for more information
 //
 // -------------------------------------------------------//
 
