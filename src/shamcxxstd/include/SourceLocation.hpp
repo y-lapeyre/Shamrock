4093e647cfd0b8c1c7c0098f6b32940e1d769ea7
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-12-23 15:17:51 +0100

    [CxxSTD] write a base format for shamrock c++ std wrapper

---
+++ src/shamcxxstd/include/SourceLocation.hpp
@@ -0,0 +1,23 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright(C) 2021-2023 Timothée David--Cléris <timothee.david--cleris@ens-lyon.fr>
+// Licensed under CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+#pragma once
+
+/**
+ * @file collectives.hpp
+ * @author Timothée David--Cléris (timothee.david--cleris@ens-lyon.fr)
+ * @brief 
+ */
+
+#include "cxxbackports/source_location.hpp"
+
+namespace shamrock::cxxstd{
+
+    using source_location = std::source_location;
+
+} // namespace shamrock::cxxstd
