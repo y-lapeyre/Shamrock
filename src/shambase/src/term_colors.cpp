edd38eafe7cd84dbfd5e3464d4b777d2394d4c0d
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-12-27 20:31:03 +0100

    [Base] fix doxygen header

--- src/shambase/src/term_colors.cpp
+++ src/shambase/src/term_colors.cpp
@@ -7,7 +7,7 @@
 // -------------------------------------------------------//
 
 /**
- * @file stacktrace.cpp
+ * @file term_colors.cpp
  * @author Timothée David--Cléris (timothee.david--cleris@ens-lyon.fr)
  * @brief 
  * 
