2075b37bcf2c6388a1ec2296bd69336079f91910
Timothée David--Cléris <timothee.davidcleri@gmail.com>
2023-12-31 12:40:39 +0100

    [Comm] fix doxygen header

---
+++ src/shamcomm/include/shamcomm/logs.hpp
@@ -0,0 +1,17 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright(C) 2021-2023 Timothée David--Cléris <timothee.david--cleris@ens-lyon.fr>
+// Licensed under CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+#pragma once
+
+/**
+ * @file logs.hpp
+ * @author Timothée David--Cléris (timothee.david--cleris@ens-lyon.fr)
+ * @brief
+ *
+ */
+
