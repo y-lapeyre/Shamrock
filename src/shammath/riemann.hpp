276092478a9f4af2859b5d4c7656eafea2d08639
Timothée David--Cléris <timothee.davidcleris@proton.me>
2024-03-06 18:11:45 +0100

    [Math] replace call to std in riemann.hpp by sham ones

--- src/shammath/riemann.hpp
+++ src/shammath/riemann.hpp
@@ -19,6 +19,7 @@
 #include <array>
 #include <cmath>
 #include <iostream>
+#include "shambackends/math.hpp"
 #include "shambackends/typeAliasVec.hpp"
 #include "shambase/sycl_utils/vectorProperties.hpp"
 namespace shammath {
@@ -139,7 +140,7 @@ namespace shammath {
     template<class Tvec>
     inline constexpr shambase::VecComponent<Tvec>
     sound_speed(PrimState<Tvec> prim, shambase::VecComponent<Tvec> gamma) {
-        return std::sqrt(gamma * prim.press / prim.rho);
+        return sycl::sqrt(gamma * prim.press / prim.rho);
     }
 
     template<class Tcons>
@@ -152,11 +153,11 @@ namespace shammath {
         const auto csL = sound_speed(primL, gamma);
         const auto csR = sound_speed(primR, gamma);
 
-        const auto S = std::max(
-            {std::abs(primL.vel[0] - csL),
-             std::abs(primR.vel[0] - csR),
-             std::abs(primL.vel[0] + csL),
-             std::abs(primR.vel[0] + csR)});
+        const auto S = sham::max(
+            {sham::abs(primL.vel[0] - csL),
+             sham::abs(primR.vel[0] - csR),
+             sham::abs(primL.vel[0] + csL),
+             sham::abs(primR.vel[0] + csR)});
 
         const auto fL = hydro_flux_x(cL, gamma);
         const auto fR = hydro_flux_x(cR, gamma);
@@ -226,8 +227,8 @@ namespace shammath {
 
         const auto csL = sound_speed(primL, gamma);
         const auto csR = sound_speed(primR, gamma);
-        const auto S_L = std::min({primL.vel[0] - csL, primR.vel[0] - csR});
-        const auto S_R = std::max({primL.vel[0] + csL, primR.vel[0] + csR});
+        const auto S_L = sham::min({primL.vel[0] - csL, primR.vel[0] - csR});
+        const auto S_R = sham::max({primL.vel[0] + csL, primR.vel[0] + csR});
 
         const auto fluxL = hydro_flux_x(consL, gamma);
         const auto fluxR = hydro_flux_x(consR, gamma);
@@ -243,8 +244,8 @@ namespace shammath {
         };
 
         auto hll_flux = [=]() {
-            const auto S_L_upwind = std::min({S_L, 0.0});
-            const auto S_R_upwind = std::max({S_R, 0.0});
+            const auto S_L_upwind = sham::min({S_L, 0.0});
+            const auto S_R_upwind = sham::max({S_R, 0.0});
             const auto S_norm     = 1.0 / (S_R_upwind - S_L_upwind);
             return (fluxL * S_R_upwind - fluxR * S_L_upwind +
                     (consR - consL) * S_R_upwind * S_L_upwind) *
