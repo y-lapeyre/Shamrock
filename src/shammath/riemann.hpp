14691cafb6ed694096c6161820ebefc2c869ac2a
Timothée David--Cléris <timothee.davidcleris@proton.me>
2024-03-06 18:16:41 +0100

    [Math] replace call to std in riemann.hpp by sham ones

--- src/shammath/riemann.hpp
+++ src/shammath/riemann.hpp
@@ -15,15 +15,16 @@
  *
  */
 
+#include "shambase/sycl_utils/vectorProperties.hpp"
+
+#include "shambackends/math.hpp"
+#include "shambackends/typeAliasVec.hpp"
+
 #include <algorithm>
 #include <array>
 #include <cmath>
 #include <iostream>
-#include "shambackends/math.hpp"
-#include "shambackends/typeAliasVec.hpp"
-#include "shambase/sycl_utils/vectorProperties.hpp"
 namespace shammath {
-
 
     template<class Tvec_>
     struct ConsState {
@@ -79,8 +80,9 @@ namespace shammath {
     };
 
     template<class Tvec>
-    inline constexpr shambase::VecComponent<Tvec> rhoekin(shambase::VecComponent<Tvec> rho, Tvec v) {
-        using Tscal = shambase::VecComponent<Tvec>;
+    inline constexpr shambase::VecComponent<Tvec>
+    rhoekin(shambase::VecComponent<Tvec> rho, Tvec v) {
+        using Tscal    = shambase::VecComponent<Tvec>;
         const Tscal v2 = v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
         return 0.5 * rho * v2;
     }
@@ -154,10 +156,8 @@ namespace shammath {
         const auto csR = sound_speed(primR, gamma);
 
         const auto S = sham::max(
-            {sham::abs(primL.vel[0] - csL),
-             sham::abs(primR.vel[0] - csR),
-             sham::abs(primL.vel[0] + csL),
-             sham::abs(primR.vel[0] + csR)});
+            sham::max(sham::abs(primL.vel[0] - csL), sham::abs(primR.vel[0] - csR)),
+            sham::max(sham::abs(primL.vel[0] + csL), sham::abs(primR.vel[0] + csR)));
 
         const auto fL = hydro_flux_x(cL, gamma);
         const auto fR = hydro_flux_x(cR, gamma);
@@ -227,8 +227,8 @@ namespace shammath {
 
         const auto csL = sound_speed(primL, gamma);
         const auto csR = sound_speed(primR, gamma);
-        const auto S_L = sham::min({primL.vel[0] - csL, primR.vel[0] - csR});
-        const auto S_R = sham::max({primL.vel[0] + csL, primR.vel[0] + csR});
+        const auto S_L = sham::min(primL.vel[0] - csL, primR.vel[0] - csR);
+        const auto S_R = sham::max(primL.vel[0] + csL, primR.vel[0] + csR);
 
         const auto fluxL = hydro_flux_x(consL, gamma);
         const auto fluxR = hydro_flux_x(consR, gamma);
@@ -244,12 +244,12 @@ namespace shammath {
         };
 
         auto hll_flux = [=]() {
-            const auto S_L_upwind = sham::min({S_L, 0.0});
-            const auto S_R_upwind = sham::max({S_R, 0.0});
+            const auto S_L_upwind = sham::min(S_L, 0.0);
+            const auto S_R_upwind = sham::max(S_R, 0.0);
             const auto S_norm     = 1.0 / (S_R_upwind - S_L_upwind);
-            return (fluxL * S_R_upwind - fluxR * S_L_upwind +
-                    (consR - consL) * S_R_upwind * S_L_upwind) *
-                   S_norm;
+            return (fluxL * S_R_upwind - fluxR * S_L_upwind
+                    + (consR - consL) * S_R_upwind * S_L_upwind)
+                   * S_norm;
         };
 
         return hll_flux();
