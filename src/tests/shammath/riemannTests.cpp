601097213506a1fcfbbb99fd09e2b58df15eec00
Timothée David--Cléris <timothee.davidcleris@proton.me>
2024-03-13 11:34:34 +0100

    [Math] add a mock stencil test to rusanov flux

--- src/tests/shammath/riemannTests.cpp
+++ src/tests/shammath/riemannTests.cpp
@@ -45,4 +45,31 @@ TestStart(Unittest, "shammath/flux_symmetry", flux_rotate, 1){
         _Assert(sham::equals(f1.rhoe,-f2.rhoe))
     }
 
+
+    Tcons state_xp = {1.1_f64, 0.8_f64, f64_3{1.1,0,0}};
+    Tcons state_yp = {1._f64, 1._f64, f64_3{1,0,0}};
+    Tcons state_zp = {1._f64, 1._f64, f64_3{1,0,0}};
+    Tcons state_i = {1._f64, 1._f64, f64_3{1,0,0}};
+    Tcons state_xm = {0.7_f64, 1.2_f64, f64_3{1.1,0,0}};
+    Tcons state_ym = {1._f64, 1._f64, f64_3{1,0,0}};
+    Tcons state_zm = {1._f64, 1._f64, f64_3{1,0,0}};
+    {
+        Tcons fx = shammath::rusanov_flux_x(state_i, state_xp, 1.6666);
+        logger::debug_ln("Riemman Solver",fx.rho, fx.rhovel, fx.rhoe); 
+        Tcons fy = shammath::rusanov_flux_y(state_i, state_yp, 1.6666);
+        logger::debug_ln("Riemman Solver",fy.rho, fy.rhovel, fy.rhoe); 
+        Tcons fz = shammath::rusanov_flux_z(state_i, state_zp, 1.6666);
+        logger::debug_ln("Riemman Solver",fz.rho, fz.rhovel, fz.rhoe); 
+        Tcons fmx = shammath::rusanov_flux_mx(state_i, state_xm, 1.6666);
+        logger::debug_ln("Riemman Solver",fmx.rho, fmx.rhovel, fmx.rhoe); 
+        Tcons fmy = shammath::rusanov_flux_my(state_i, state_ym, 1.6666);
+        logger::debug_ln("Riemman Solver",fmy.rho, fmy.rhovel, fmy.rhoe); 
+        Tcons fmz = shammath::rusanov_flux_mz(state_i, state_zm, 1.6666);
+        logger::debug_ln("Riemman Solver",fmz.rho, fmz.rhovel, fmz.rhoe); 
+        Tcons sum = fx + fy +fz+fmx+fmy+fmz;
+        logger::debug_ln("Riemman Solver","sum=",sum.rho, sum.rhovel, sum.rhoe); 
+        _Assert(sum.rhovel[1] == 0)
+        _Assert(sum.rhovel[2] == 0)
+    }
+
 }
