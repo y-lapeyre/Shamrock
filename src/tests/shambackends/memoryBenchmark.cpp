f919c6cf65c9436460d0f8fbc1af6235a8789987
Timothée David--Cléris <timothee.davidcleris@proton.me>
2024-03-19 13:10:27 +0100

    [Test] fix copyright

--- src/tests/shambackends/memoryBenchmark.cpp
+++ src/tests/shambackends/memoryBenchmark.cpp
@@ -1,3 +1,11 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright(C) 2021-2023 Timothée David--Cléris <timothee.david--cleris@ens-lyon.fr>
+// Licensed under CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
 #include "shamcomm/logs.hpp"
 #include "shamsys/NodeInstance.hpp"
 #include "shamtest/shamtest.hpp"
