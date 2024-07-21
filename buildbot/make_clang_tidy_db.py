d3f516fd87ebba4a08e6f007fc8cd1adc07074e5
David--Cléris Timothée <timothee.davidcleris@proton.me>
2024-07-21 19:17:39 +0200

    [CI] add filter to clang tidy job (#520)

--- buildbot/make_clang_tidy_db.py
+++ buildbot/make_clang_tidy_db.py
@@ -6,6 +6,23 @@ db = comp_db.read()
 db = db.replace("--acpp-targets='omp'","")
 #print(db)
 
+
+def remove_external_files():
+    global db
+    
+    import json
+    dic = json.loads(db)
+
+    ret_dic = []
+
+    for a in dic:
+        if not ("Shamrock/external" in a["file"]):
+            ret_dic.append(a)
+
+    db = json.dumps(ret_dic)
+
+remove_external_files()
+
 try:
     os.mkdir("build/clang-tidy.mod")
 except:
