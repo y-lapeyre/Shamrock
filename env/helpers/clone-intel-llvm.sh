a21e5294768ea86106e6a22d5a0b9fa26b5610b6
David--Cléris Timothée <timothee.davidcleris@proton.me>
2024-10-11 17:05:56 +0200

    [Env] add custom mpi case for dgx env (#619)

---
+++ env/helpers/clone-intel-llvm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ -z ${INTELLLVM_GIT_DIR+x} ]; then echo "INTELLLVM_GIT_DIR is unset"; return; fi
+
+if [ ! -f "$INTELLLVM_GIT_DIR/README.md" ]; then
+    echo " ------ Clonning LLVM ------ "
+
+    if [ -z ${INTEL_LLVM_VERSION+x} ]
+    then
+        git clone -b sycl https://github.com/intel/llvm.git $INTELLLVM_GIT_DIR
+        (cd $INTELLLVM_GIT_DIR && git checkout $INTEL_LLVM_VERSION)
+    else
+        git clone -b sycl https://github.com/intel/llvm.git $INTELLLVM_GIT_DIR
+    fi
+    echo " ------  LLVM Cloned  ------ "
+
+fi
