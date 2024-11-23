7593375a2d70ba8bcafba5651d97a241f2a4e982
David--Cléris Timothée <timothee.davidcleris@proton.me>
2024-11-23 08:17:41 +0100

    [CI] improve clang-tidy db gen (#673)

--- buildbot/make_clang_tidy_db.py
+++ buildbot/make_clang_tidy_db.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 comp_db = open("build/compile_commands.json", "r")
 db = comp_db.read()
@@ -6,6 +7,19 @@ db = comp_db.read()
 db = db.replace("--acpp-targets='omp'","")
 #print(db)
 
+def remove_plugin_flags(cmd):
+
+    new_cmd = ""
+
+    for a in cmd.split():
+
+        if not (a.startswith("-fplugin=") or a.startswith("-fpass-plugin")):
+            new_cmd += a
+            new_cmd += " "
+
+    #print(new_cmd)
+
+    return new_cmd
 
 def remove_external_files():
     global db
@@ -17,9 +31,13 @@ def remove_external_files():
 
     for a in dic:
         if not ("Shamrock/external" in a["file"]):
+            cmd = a["command"] + " --acpp-dryrun"
+            #print("--->",cmd)
+            new_cmd = os.popen(cmd).readlines()[0][:-1]
+            a["command"] = remove_plugin_flags(new_cmd)
             ret_dic.append(a)
 
-    db = json.dumps(ret_dic)
+    db = json.dumps(ret_dic, indent=4)
 
 remove_external_files()
 
