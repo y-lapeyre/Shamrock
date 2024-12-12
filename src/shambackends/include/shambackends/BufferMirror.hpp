6bc593c1fc64bbc4a8b2bcd15fb8c5db3106fe81
Timothée David--Cléris <timothee.davidcleris@proton.me>
2024-12-12 12:02:58 +0100

    document the class

--- src/shambackends/include/shambackends/BufferMirror.hpp
+++ src/shambackends/include/shambackends/BufferMirror.hpp
@@ -20,17 +20,50 @@
 #include "shambackends/DeviceBuffer.hpp"
 #include "shambackends/EventList.hpp"
 #include "shamcomm/logs.hpp"
+
 namespace sham {
 
+    /**
+     * @brief A class template for creating a mirrored buffer
+     *
+     * This class provides a mechanism to create a mirrored buffer of a specific
+     * DeviceBuffer that can be accessed like a normal pointer.
+     *
+     * Exemple of usage :
+     * \code{.cpp}
+     * {
+     *     auto acc = f.get_buf().template mirror_to<sham::host>();
+     *     auto acc_xyz = xyz.get_buf().template mirror_to<sham::host>();
+     * 
+     *     for (u32 i = 0; i < f.size(); i++) {
+     *         Tvec position = acc_xyz[i];
+     *         acc[i] =function(position);
+     *     }
+     * 
+     * }
+     * \endcode
+     *
+     * @tparam T The type of elements in the buffer
+     * @tparam target The USM kind target for the mirror buffer
+     * @tparam orgin_target The original USM kind target of the mirrored buffer
+     */
     template<class T, USMKindTarget target, USMKindTarget orgin_target>
     class BufferMirror {
 
-        DeviceBuffer<T, orgin_target> &mirrored_buffer;
+        DeviceBuffer<T, orgin_target> &mirrored_buffer; ///< Reference to the original buffer
 
-        DeviceBuffer<T, target> mirror;
-        T *ptr_mirror;
+        DeviceBuffer<T, target> mirror; ///< The mirrored buffer
+        T *ptr_mirror;                  ///< Pointer to the mirrored data
 
         public:
+        /**
+         * @brief Constructs a BufferMirror
+         *
+         * Initializes the mirrored buffer of the original data and retain a reference to the
+         * original buffer
+         *
+         * @param mirrored_buffer The original buffer to be mirrored
+         */
         BufferMirror(DeviceBuffer<T, orgin_target> &mirrored_buffer)
             : mirrored_buffer(mirrored_buffer), mirror(mirrored_buffer.template copy_to<target>()) {
             sham::EventList depends_list;
@@ -39,17 +72,39 @@ namespace sham {
             mirror.complete_event_state(sycl::event{});
         }
 
-        BufferMirror(const BufferMirror &)            = delete;
-        BufferMirror(BufferMirror &&)                 = delete;
-        BufferMirror &operator=(const BufferMirror &) = delete;
-        BufferMirror &operator=(BufferMirror &&)      = delete;
+        BufferMirror(const BufferMirror &)            = delete; //< Please don't
+        BufferMirror(BufferMirror &&)                 = delete; //< Please don't
+        BufferMirror &operator=(const BufferMirror &) = delete; //< Please don't
+        BufferMirror &operator=(BufferMirror &&)      = delete; //< Please don't
 
+        /**
+         * @brief Provides access to the mirrored data
+         *
+         * @return Pointer to the mirrored data
+         */
         T *data() const { return ptr_mirror; }
+
+        /**
+         * @brief Returns the size of the mirrored buffer
+         *
+         * @return Size of the mirrored buffer
+         */
         u32 size() const { return mirrored_buffer.size(); }
 
+        /**
+         * @brief Accesses the mirrored data at a given index
+         *
+         * @param i Index of the data to access
+         * @return Reference to the data at the given index
+         */
         T &operator[](u32 i) const { return ptr_mirror[i]; }
 
-        ~BufferMirror() { mirrored_buffer.template copy_from(mirror); };
+        /**
+         * @brief Destructor
+         *
+         * Copies the data from the mirror back to the original buffer.
+         */
+        ~BufferMirror() { mirrored_buffer.template copy_from(mirror); }
     };
 
 } // namespace sham
