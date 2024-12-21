9abf11bf4f86e6c46782495cef6b97e9456349fe
Timothée David--Cléris <timothee.davidcleris@proton.me>
2024-12-21 22:03:58 +0100

    [Backends] fix misreporting of max memory usage

--- src/shambackends/src/details/memoryHandle.cpp
+++ src/shambackends/src/details/memoryHandle.cpp
@@ -32,8 +32,8 @@ namespace {
 
         mem_perf_infos.allocated_byte_device += size;
         mem_perf_infos.time_alloc_device += timed;
-        mem_perf_infos.max_allocated_byte_device
-            = std::max(mem_perf_infos.max_allocated_byte_device, mem_perf_infos.allocated_byte_device);
+        mem_perf_infos.max_allocated_byte_device = std::max(
+            mem_perf_infos.max_allocated_byte_device, mem_perf_infos.allocated_byte_device);
 
         shambase::profiling::register_counter_val(
             "Device Memory", shambase::details::get_wtime(), mem_perf_infos.allocated_byte_device);
@@ -45,8 +45,8 @@ namespace {
 
         mem_perf_infos.allocated_byte_shared += size;
         mem_perf_infos.time_alloc_shared += timed;
-        mem_perf_infos.max_allocated_byte_shared
-            = std::max(mem_perf_infos.max_allocated_byte_shared, mem_perf_infos.allocated_byte_shared);
+        mem_perf_infos.max_allocated_byte_shared = std::max(
+            mem_perf_infos.max_allocated_byte_shared, mem_perf_infos.allocated_byte_shared);
 
         shambase::profiling::register_counter_val(
 
