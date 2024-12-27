01eacc0f42df7b10c5b7a9079421a06eadff64c0
Timothée David--Cléris <timothee.davidcleris@proton.me>
2024-12-27 15:42:58 +0100

    add type checking in kernel call

---
+++ src/shambase/include/shambase/optional.hpp
@@ -0,0 +1,26 @@
+// -------------------------------------------------------//
+//
+// SHAMROCK code for hydrodynamics
+// Copyright (c) 2021-2024 Timothée David--Cléris <tim.shamrock@proton.me>
+// SPDX-License-Identifier: CeCILL Free Software License Agreement v2.1
+// Shamrock is licensed under the CeCILL 2.1 License, see LICENSE for more information
+//
+// -------------------------------------------------------//
+
+#pragma once
+
+/**
+ * @file optional.hpp
+ * @author Timothée David--Cléris (tim.shamrock@proton.me)
+ * @brief
+ *
+ */
+
+#include <optional>
+
+namespace shambase {
+
+    /// @brief Optional reference wrapper
+    template<typename T>
+    using opt_ref = std::optional<std::reference_wrapper<T>>;
+} // namespace shambase
