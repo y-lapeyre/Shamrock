871e08cd6d3fac2fa7256cdad24a862021b125ab
David--Cléris Timothée <timothee.davidcleris@proton.me>
2025-02-03 21:50:06 +0100

    [Env][conda.acpp] enfore the correct cpu compiler (#784)
    fixes #781.

--- env/machine/conda/acpp/conda_acpp_env.sh
+++ env/machine/conda/acpp/conda_acpp_env.sh
@@ -16,6 +16,9 @@ echo " --------- Activating conda environment --------- "
 conda activate shamrock_dev_environment
 echo " ------------- Environment activated ------------ "
 
+#enfore the use of the correct clang++ as the installation of acpp register a buggy compiler
+export ACPP_CPU_CXX=$(which clang++)
+
 function shamconfigure {
     cmake \
         -S $SHAMROCK_DIR \
@@ -26,7 +29,7 @@ function shamconfigure {
         -DCMAKE_C_COMPILER="clang" \
         -DCMAKE_MAKE_PROGRAM=$(which ninja) \
         -DUSE_SYSTEM_FMTLIB=On \
-        -DCMAKE_CXX_FLAGS="${SHAMROCK_CXX_FLAGS}" \
+        -DCMAKE_CXX_FLAGS="${SHAMROCK_CXX_FLAGS} --acpp-cpu-cxx=$(which clang++)" \
         -DACPP_PATH=$(which acpp) \
         -DCMAKE_BUILD_TYPE="${SHAMROCK_BUILD_TYPE}" \
         -DBUILD_TEST=Yes \
