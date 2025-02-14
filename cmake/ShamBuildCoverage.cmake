67d4dd9805ad1063a05f6e7e83adaf44a7f61e2a
David--Cléris Timothée <timothee.davidcleris@proton.me>
2025-02-14 22:38:18 +0100

    [CI] fix missing functions in coverage and update llvm (#810)

--- cmake/ShamBuildCoverage.cmake
+++ cmake/ShamBuildCoverage.cmake
@@ -11,3 +11,13 @@ set(coverage_flags "-fprofile-instr-generate -fcoverage-mapping")
 
 set(CMAKE_C_FLAGS_COVERAGE "${coverage_flags}")
 set(CMAKE_CXX_FLAGS_COVERAGE "${coverage_flags}")
+
+# disable shared lib if coverage is enabled
+if(CMAKE_BUILD_TYPE STREQUAL "COVERAGE")
+    if(SHAMROCK_USE_SHARED_LIB)
+    message(STATUS
+        "forcing SHAMROCK_USE_SHARED_LIB=Off with coverage enabled, "
+        "without it llvm-cov will show only the main binary functions")
+    endif()
+    set(SHAMROCK_USE_SHARED_LIB Off)
+endif()
