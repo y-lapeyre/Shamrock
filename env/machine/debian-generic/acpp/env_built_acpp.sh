826020b169dffffe8670a8ee0c0eab1d05016850
David--Cléris Timothée <timothee.davidcleris@proton.me>
2025-05-12 19:49:31 +0200

    [Env] update acpp 24.10.0 -> 25.02.0 (#972)

--- env/machine/debian-generic/acpp/env_built_acpp.sh
+++ env/machine/debian-generic/acpp/env_built_acpp.sh
@@ -1,6 +1,6 @@
 # Everything before this line will be provided by the new-env script
 
-export ACPP_VERSION=v24.10.0
+export ACPP_VERSION=v25.02.0
 export ACPP_APPDB_DIR=/tmp/acpp-appdb # otherwise it would we in the $HOME/.acpp
 export ACPP_GIT_DIR=$BUILD_DIR/.env/acpp-git
 export ACPP_BUILD_DIR=$BUILD_DIR/.env/acpp-builddir
