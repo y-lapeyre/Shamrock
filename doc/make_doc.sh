ad8363830e17997a548299edc0359722f5c6d62e
David--Cléris Timothée <timothee.davidcleris@proton.me>
2025-06-23 17:48:06 +0200

    [Doc] return error on doxygen crash (#1067)

--- doc/make_doc.sh
+++ doc/make_doc.sh
@@ -1,5 +1,20 @@
 cd doxygen
-doxygen dox.conf
+for i in {1..3}
+do
+    doxygen dox.conf
+    if [ $? -eq 0 ]
+    then
+        break
+    fi
+    echo "Doxygen failed, retrying ($i/3)"
+done
+if [ $i -eq 3 ]
+then
+    echo "Doxygen failed three times, giving up."
+    exit 1
+fi
+
+set -e
 
 cd ../mkdocs
 cd docs/assets/figures
@@ -8,11 +23,9 @@ cd ../../..
 mkdocs build
 cd ..
 
-
 rm -rf _build
 mkdir _build
 cd _build
-
 
 mkdir doxygen
 mkdir mkdocs
