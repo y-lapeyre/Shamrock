e818713b58cc7577aa1d8c94fdc053c02986d81f
Yona Lapeyre <yona.lapeyre@ens-lyon.fr>
2025-07-30 12:05:55 +0200

    fixed the issue, missing f64_3 instanciation

--- src/shammodels/sph/src/modules/render/RenderFieldGetter.cpp
+++ src/shammodels/sph/src/modules/render/RenderFieldGetter.cpp
@@ -88,3 +88,9 @@ template class shammodels::sph::modules::RenderFieldGetter<f64_3, f64, M8>;
 template class shammodels::sph::modules::RenderFieldGetter<f64_3, f64, C2>;
 //template class shammodels::sph::modules::RenderFieldGetter<f64_3, f64_3, C4>;
 //template class shammodels::sph::modules::RenderFieldGetter<f64_3, f64_3, C6>;
+
+template class shammodels::sph::modules::RenderFieldGetter<f64_3, f64_3, M4>;
+template class shammodels::sph::modules::RenderFieldGetter<f64_3, f64_3, M6>;
+template class shammodels::sph::modules::RenderFieldGetter<f64_3, f64_3, M8>;
+
+template class shammodels::sph::modules::RenderFieldGetter<f64_3, f64_3, C2>;
