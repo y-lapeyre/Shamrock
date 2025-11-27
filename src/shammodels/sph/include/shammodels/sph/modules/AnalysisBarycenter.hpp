610063479d5f95c9f64ec34798cb68bbb3eb228e
David Fang <63859514+DavidFang03@users.noreply.github.com>
2025-11-27 21:48:25 +0100

    [Git] mailmap update (#1377)
    Co-authored-by: David--Cléris Timothée <timothee.davidcleris@proton.me>

--- src/shammodels/sph/include/shammodels/sph/modules/AnalysisBarycenter.hpp
+++ src/shammodels/sph/include/shammodels/sph/modules/AnalysisBarycenter.hpp
@@ -11,7 +11,7 @@
 
 /**
  * @file AnalysisBarycenter.hpp
- * @author David Fang (fang.david03@gmail.com)
+ * @author David Fang (david.fang@ikmail.com)
  * @author Timothée David--Cléris (tim.shamrock@proton.me)
  * @brief AnalysisBarycenter class with one method AnalysisBarycenter.get_barycenter()
  *
