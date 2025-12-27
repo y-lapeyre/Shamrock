c2c8d9588aef6bf5c921d13c467b607d52c17078
Timothée David--Cléris <timothee.davidcleris@proton.me>
2025-12-27 11:50:28 +0100

    [authorship] fix authorship

--- src/shammodels/ramses/include/shammodels/ramses/modules/SlopeLimitedGradientUtilities.hpp
+++ src/shammodels/ramses/include/shammodels/ramses/modules/SlopeLimitedGradientUtilities.hpp
@@ -12,7 +12,7 @@
 /**
  * @file SlopeLimitedGradientUtilities.hpp
  * @author Léodasce Sewanou (leodasce.sewanou@ens-lyon.fr)
- * @author Timothée David--Cléris (tim.shamrock@proton.me) --no git blame--
+ * @author Timothée David--Cléris (tim.shamrock@proton.me)
  * @brief
  *
  */
