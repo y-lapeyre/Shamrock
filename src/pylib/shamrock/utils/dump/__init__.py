84506a6041d061eb729223eeda78a4ed38203728
David--Cléris Timothée <timothee.davidcleris@proton.me>
2026-06-16 14:38:23 +0200

    [Pylib] cleanup & document ShamrockDumpHandleHelper (#1877)

--- src/pylib/shamrock/utils/dump/__init__.py
+++ src/pylib/shamrock/utils/dump/__init__.py
@@ -4,7 +4,10 @@ import os
 import shamrock.sys
 
 
-def purge_old_dumps(dump_prefix, keep_first=1, keep_last=3, ext=".sham"):
+def helper_purge_old_dumps(dump_prefix, keep_first=1, keep_last=3, ext=".sham") -> None:
+    """
+    Purge old dump files.
+    """
     if shamrock.sys.world_rank() == 0:
         res = glob.glob(dump_prefix + "*" + ext)
         res.sort()
@@ -16,7 +19,10 @@ def purge_old_dumps(dump_prefix, keep_first=1, keep_last=3, ext=".sham"):
             os.remove(f)
 
 
-def get_last_dump(dump_prefix, ext=".sham"):
+def helper_get_last_dump(dump_prefix, ext=".sham") -> int | None:
+    """
+    Get the last dump number.
+    """
     res = glob.glob(dump_prefix + "*" + ext)
 
     num_max = -1
@@ -36,36 +42,103 @@ def get_last_dump(dump_prefix, ext=".sham"):
 
 
 class ShamrockDumpHandleHelper:
+    """
+    Helper class to handle dump files.
+    """
+
     def __init__(self, model, dump_prefix, ext=".sham"):
         self.model = model
         self.dump_prefix = dump_prefix
         self.ext = ext
         os.makedirs(os.path.dirname(self.dump_prefix), exist_ok=True)
 
-    def get_dump_name(self, idump):
-        return self.dump_prefix + f"{idump:07}" + self.ext
+    def get_dump_name_extension(self, idump, ext) -> str:
+        """Get the name of the dump file with the extension"""
+        return self.dump_prefix + f"{idump:07}" + ext
 
-    def get_last_dump(self):
-        return get_last_dump(self.dump_prefix, self.ext)
+    def get_dump_name(self, idump) -> str:
+        """Get the name of the dump file (extension from self.ext)"""
+        return self.get_dump_name_extension(idump, self.ext)
 
-    def purge_old_dumps(self, keep_first=1, keep_last=3):
-        purge_old_dumps(self.dump_prefix, keep_first, keep_last, self.ext)
+    def get_last_dump(self) -> int | None:
+        """Find the last dump number"""
+        return helper_get_last_dump(self.dump_prefix, self.ext)
 
-    def load_dump(self, idump):
+    def purge_old_dumps(self, keep_first=1, keep_last=3) -> None:
+        """
+        Purge old dump files.
+
+        Parameters
+        ----------
+        keep_first : int, optional
+            Number of oldest dump files to keep (default is 1, i.e. keep the first dump).
+        keep_last : int, optional
+            Number of newest dump files to keep (default is 3, i.e. keep the last 3 dumps).
+
+        Returns
+        -------
+        None
+            This method does not return a value.
+        """
+        helper_purge_old_dumps(self.dump_prefix, keep_first, keep_last, self.ext)
+
+    def load_dump(self, idump) -> None:
+        """
+        Load a dump file.
+
+        Parameters
+        ----------
+        idump : int
+            The dump identifier to load.
+
+        Returns
+        -------
+        None
+            This method does not return a value.
+        """
         dump_name = self.get_dump_name(idump)
         if shamrock.sys.world_rank() == 0:
             print(f"Loading dump: {dump_name} i={idump}")
         self.model.load_from_dump(dump_name)
 
-    def write_dump(self, idump, purge_old_dumps=False, keep_first=1, keep_last=3):
+    def write_dump(self, idump, purge_old_dumps=False, keep_first=1, keep_last=3) -> None:
+        """
+        Write a dump file.
+
+        Parameters
+        ----------
+        idump : int
+            The dump identifier to write.
+        purge_old_dumps : bool, optional
+            Whether to purge old dumps (default is False).
+        keep_first : int, optional
+            Number of oldest dump files to keep (default is 1, i.e. keep the first dump).
+        keep_last : int, optional
+            Number of newest dump files to keep (default is 3, i.e. keep the last 3 dumps).
+
+        Returns
+        -------
+        None
+            This method does not return a value.
+        """
         dump_name = self.get_dump_name(idump)
         self.model.dump(dump_name)
         if purge_old_dumps:
             self.purge_old_dumps(keep_first, keep_last)
 
-    def load_last_dump_or(self, functor_no_last_dump):
+    def load_last_dump_or(self, functor_no_last_dump) -> None:
+        """
+        Load the last dump or call a function if no dump is found.
+
+        Parameters
+        ----------
+        functor_no_last_dump : callable
+            The function to call if no dump are found (i.e. the setup function).
+        """
         idump = self.get_last_dump()
         if idump is None:
-            return functor_no_last_dump()
+            result = functor_no_last_dump()
+            if result is not None:
+                raise ValueError("functor_no_last_dump must not return a value")
         else:
-            return self.load_dump(idump)
+            self.load_dump(idump)
