97743d818862eb92e7c1e363828ab83c40f54b19
David--Cléris Timothée <timothee.davidcleris@proton.me>
2026-06-26 14:32:18 +0200

    [autofix] automatic fix: pre-commit hooks and author updates (#1899)
    ## Auto-generated fixes
    
    This PR contains automatic fixes from pre-commit hooks and author
    updates.
    
    **Triggered by**: push on `main`
    **Commit**: eaa1303ba832caa4c27e93a48f06fb13a1bb7f3e
    
    ### Changes:
    - Pre-commit hook fixes
    - Author information updates
    
    Please review the changes before merging.

--- src/shammodels/common/include/shammodels/common/ExtForceConfig.hpp
+++ src/shammodels/common/include/shammodels/common/ExtForceConfig.hpp
@@ -12,6 +12,7 @@
 /**
  * @file ExtForceConfig.hpp
  * @author Timothée David--Cléris (tim.shamrock@proton.me)
+ * @author Yona Lapeyre (yona.lapeyre@ens-lyon.fr)
  * @brief
  *
  */
--- src/shammodels/sph/include/shammodels/sph/modules/ExternalForces.hpp
+++ src/shammodels/sph/include/shammodels/sph/modules/ExternalForces.hpp
@@ -12,6 +12,7 @@
 /**
  * @file ExternalForces.hpp
  * @author Timothée David--Cléris (tim.shamrock@proton.me)
+ * @author Yona Lapeyre (yona.lapeyre@ens-lyon.fr)
  * @brief
  *
  */
