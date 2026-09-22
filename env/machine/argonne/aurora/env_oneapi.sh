90f2abde87ac15a2fbf09cdfe475a736a099c991
David--Cléris Timothée <timothee.davidcleris@proton.me>
2026-09-22 03:36:50 +0200

    [Env] Switch aurora config to AOT compile (#2419)
    Add -fsycl-targets=spir64_gen -Xsycl-target-backend "-device pvc", -fno-system-debug, and --offload-compress to CMAKE_CXX_FLAGS, and -flink-huge-device-code to CMAKE_EXE_LINKER_FLAGS.
    
    This make dpcpp use ahead of time compiling rather than JIT.

--- env/machine/argonne/aurora/env_oneapi.sh
+++ env/machine/argonne/aurora/env_oneapi.sh
@@ -16,8 +16,8 @@ function shamconfigure {
         -DINTEL_LLVM_PATH=$(dirname $(which icpx))/.. \
         -DCMAKE_CXX_COMPILER=$(which icpx) \
         -DCMAKE_C_COMPILER=$(which icx) \
-        -DCMAKE_CXX_FLAGS="-g -fsycl -fp-model=precise" \
-        -DCMAKE_EXE_LINKER_FLAGS="-Wl,--copy-dt-needed-entries" \
+        -DCMAKE_CXX_FLAGS="-g -fsycl -fsycl-targets=spir64_gen -Xsycl-target-backend \"-device pvc\" -fp-model=precise -fno-system-debug --offload-compress " \
+        -DCMAKE_EXE_LINKER_FLAGS="-Wl,--copy-dt-needed-entries -flink-huge-device-code" \
         -DCMAKE_BUILD_TYPE="${SHAMROCK_BUILD_TYPE}" \
         -DBUILD_TEST=Yes \
         -DSHAMROCK_USE_CPPTRACE=Yes \
